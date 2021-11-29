c48d01b62c806393d1662448c74927358848e2cb
ChethanKottari <chethankottari789@gmail.com>
2021-11-29 13:10:20 +0530

    working on bootstrap and api intergration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Final Year Project';
+  //title = 'Final Year Project';
 }
